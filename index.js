e998a791a3a97e9feee0b2657d29e405e3e5a3bf
Dmitry Pustovit <me@dmitrypustovit.com>
2015-05-26 09:55:34 -0400

    Test

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-$('#tileGrid td').hover(
-       function(){ $(this).addClass('animated shake') },
-       function(){ $(this).removeClass('animated shake') }
-)
+function fixTiling() {
+    var closestTW = $( window ).width()  / 7 - 2;
+    $(".tile").css({width: closestTW, height: closestTW}); 
+    $(".dw").css({width: 2 * closestTW + 2}); 
+    $(".dh").css({height: 2 * closestTW + 2}); 
+    $(".tw").css({width: 3 * closestTW + 4}); 
+    $(".th").css({height: 3 * closestTW + 4}); 
+}
+
+fixTiling();
+
+$(window).resize(function() {
+  fixTiling();
+});
+
+$("#container").packery({
+  itemSelector: '.tile',
+  gutter: 2
+});
